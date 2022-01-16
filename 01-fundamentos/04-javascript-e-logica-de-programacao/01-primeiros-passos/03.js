4b4dd22dd1a0b8216a04bef26508dba69c7f7bf3
Vinicius <hello@marvig.dev>
2022-01-16 17:41:08 +0000

    [4.1] Resolução - Exercício 3

---
+++ 01-fundamentos/04-javascript-e-logica-de-programacao/01-primeiros-passos/03.js
@@ -0,0 +1,15 @@
+const a = 11, b = 10, c = 7;
+
+if(a > b){
+  if(a > c) {
+    console.log(a)
+  } else {
+    console.log(c);
+  }
+}else{
+  if (b > c){
+    console.log(b);
+  }else{
+    console.log(c);
+  }
+}
