fe31ed28591e0d4d08f54128d62ac99974378a3d
Marcos Vinicius <hello@marvig.dev>
2022-01-20 08:29:07 -0300

    [8.3] Resolução - Requisito 04

---
+++ 01-fundamentos/08-higher-order-functions/03-map-e-filter/tests/04.spec.js
@@ -0,0 +1,31 @@
+const oldBooksOrdered = require('../src/04');
+
+const expected = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+describe('04 - Retornar array apenas com livros com mais de 60 anos de publicação, ordenando-os', () => {
+  it('Deve retornar o array esperado', () => {
+    expect(oldBooksOrdered()).toEqual(expected);
+  });
+});
